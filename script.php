8a0d6fd00cc9fd63a0a5e89e9baeb9cf5cb065f7
GvMazzon25 <gvmazzon25@gmail.com>
2022-01-10 17:28:15 +0100

    set-products

--- script.php
+++ script.php
@@ -14,8 +14,8 @@
   class UserSpecial extends User{
       public $premium;
 
-      public function setPremio($premium){
-          $this->livello = $livello;
+      public function setPremium($premium){
+          $this->premium = $premium;
       }
 
       public function setSconto($premium){
@@ -41,4 +41,12 @@
   $premium_client->$name = 'Antonio';
   $premium_client->$mail = 'amazzon74@gmail.com';
   $premium_client->$sconto = setSconto(false)
+
+  $prodotto1 = new Product()
+  $prodotto1->$title = 't-shirt';
+  $prodotto1->$price = '20';
+
+  $prodotto2 = new Product()
+  $prodotto2->$title = 'jeans';
+  $prodotto2->$price = '50';
 ?>
