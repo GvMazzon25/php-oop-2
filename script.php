91b140478462bd5e3f90ce6cf44aab84b1da50fb
GvMazzon25 <gvmazzon25@gmail.com>
2022-01-10 16:55:38 +0100

    start-project

---
+++ script.php
@@ -0,0 +1,35 @@
+<?php
+  class User{
+      public $name;
+      public $mail;
+      public $sconto = 0;
+
+      public function setSconto($premium){
+         if($premium === true) {
+             $this->sconto = 15;
+         }
+      }
+  }
+
+  class Product{
+      public $title;
+      public $price;
+  }
+
+  class UserSpecial extends User{
+      public $premium;
+
+      public function setPremio($premium){
+          $this->livello = $livello;
+      }
+
+      public function setSconto($premium){
+        if($premium === true) {
+            $this->sconto = 50;
+        }else {
+            $this->sconto = $this->livello *10;
+        }
+     }
+  }
+
+?>
